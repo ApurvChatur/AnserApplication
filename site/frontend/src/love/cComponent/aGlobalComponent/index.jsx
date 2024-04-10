e2c3bc5292aaac4e15c8e86216d7146b6630240a
ApurvChatur <chaturapurv@gmail.com>
2024-04-10 17:51:45 +0530

    Auth and Profile fixed

--- site/frontend/src/love/cComponent/aGlobalComponent/index.jsx
+++ site/frontend/src/love/cComponent/aGlobalComponent/index.jsx
@@ -13,7 +13,9 @@ const GlobalComponent = ({ Redux, LogoutAPICall, children }) => {
       <Suspense fallback={null}>
         <HeaderComponent Redux={Redux} LogoutAPICall={LogoutAPICall} />
       </Suspense>
-      { children }
+      <div className='min-h-screen bg-[#E2D3F3] dark:bg-[#013DC4] text-[#013DC4] dark:text-[#E2D3F3]'>
+        { children }
+      </div>
       <Suspense fallback={null}>
         <FooterComponent Redux={Redux} />
       </Suspense>
